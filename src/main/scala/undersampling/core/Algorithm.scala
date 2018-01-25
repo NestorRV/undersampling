898d6f09a16945a5a0fd3d2bb937cc1850a34dce
Néstor Rodríguez <nestorrodriguezvico@gmail.com>
2018-01-25 20:22:33 +0100

    Possibility to shuffle the data. Add doc.

--- src/main/scala/undersampling/core/Algorithm.scala
+++ src/main/scala/undersampling/core/Algorithm.scala
@@ -2,14 +2,22 @@ package undersampling.core
 
 import scala.util.Random
 
+/** Base class to all the algorithms
+  *
+  * @param x             data to work with
+  * @param y             labels of the data associated with x
+  * @param seed          seed to use. If it is not provided, it will use the system time
+  * @param minorityClass integer representing the class to keep it untouched
+  * @author Néstor Rodríguez Vico
+  */
 class Algorithm(private[undersampling] val x: Array[Array[Double]], private[undersampling] val y: Array[Int],
                 private[undersampling] val seed: Long = System.currentTimeMillis(), minorityClass: Int = 0) {
 
-  private[undersampling] val random: Random = new util.Random(seed)
   // Shuffle the data to make it random
-  private[undersampling] val index: List[Int] = this.random.shuffle(this.y.indices.toList)
-  private[undersampling] val randomizedX: Array[Array[Double]] = (this.index map this.x).toArray
-  private[undersampling] val randomizedY: Array[Int] = (this.index map this.y).toArray
+  private[undersampling] var random: Random = new util.Random(seed)
+  private[undersampling] var index: List[Int] = this.random.shuffle(this.y.indices.toList)
+  private[undersampling] var randomizedX: Array[Array[Double]] = (this.index map this.x).toArray
+  private[undersampling] var randomizedY: Array[Int] = (this.index map this.y).toArray
   // Count the number of instances for each class
   private[undersampling] val counter: Array[(Int, Int)] = this.randomizedY.groupBy((l: Int) => l).map((t: (Int, Array[Int])) => (t._1, t._2.length)).toArray.sortBy { case (_, d) => d }
   // In certain algorithms, reduce the minority class is forbidden, so let's detect what class is it if the user don't
@@ -22,6 +30,19 @@ class Algorithm(private[undersampling] val x: Array[Array[Double]], private[unde
   private[undersampling] val maxV: Array[Double] = this.randomizedX.transpose.map((x: Array[Double]) => x.max)
   private[undersampling] val minV: Array[Double] = this.randomizedX.transpose.map((x: Array[Double]) => x.min)
   // Normalize the data as follow: for each column, x, (x-min(x))/(max(x)-min(x))
-  private[undersampling] val normalizedData: Array[Array[Double]] = (this.randomizedX.transpose zip (this.minV zip this.maxV)).map((row: (Array[Double], (Double, Double))) =>
+  private[undersampling] var normalizedData: Array[Array[Double]] = (this.randomizedX.transpose zip (this.minV zip this.maxV)).map((row: (Array[Double], (Double, Double))) =>
     row._1.map((e: Double) => e - row._2._1 / (row._2._2 / row._2._1))).transpose
+
+  /** Shuffle the data
+    *
+    * @param seed seed to use in the Random object used to shuffle
+    */
+  def shuffle(seed: Long): Unit = {
+    this.random = new util.Random(seed)
+    this.index = this.random.shuffle(this.y.indices.toList)
+    this.randomizedX = (this.index map this.x).toArray
+    this.randomizedY = (this.index map this.y).toArray
+    this.normalizedData = (this.randomizedX.transpose zip (this.minV zip this.maxV)).map((row: (Array[Double], (Double, Double))) =>
+      row._1.map((e: Double) => e - row._2._1 / (row._2._2 / row._2._1))).transpose
+  }
 }
