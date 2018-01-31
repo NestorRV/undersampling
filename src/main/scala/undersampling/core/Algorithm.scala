db7d7d5ac5c0df6123e0c84e34f43dcd5d317fb5
Néstor Rodríguez <nestorrodriguezvico@gmail.com>
2018-02-01 00:16:08 +0100

    Fix: randomizedX is now normalized in Algorithm.

--- src/main/scala/undersampling/core/Algorithm.scala
+++ src/main/scala/undersampling/core/Algorithm.scala
@@ -17,24 +17,24 @@ private[undersampling] class Algorithm(private[undersampling] val x: Array[Array
 
   // Logger object to log the execution of the algorithms
   private[undersampling] val logger = new Logger
-  // Shuffle the data to make it random
-  private[undersampling] var random: Random = new util.Random(seed)
-  private[undersampling] var index: List[Int] = this.random.shuffle(this.y.indices.toList)
-  private[undersampling] var randomizedX: Array[Array[Double]] = (this.index map this.x).toArray
-  private[undersampling] var randomizedY: Array[Int] = (this.index map this.y).toArray
   // Count the number of instances for each class
-  private[undersampling] val counter: Array[(Int, Int)] = this.randomizedY.groupBy((l: Int) => l).map((t: (Int, Array[Int])) => (t._1, t._2.length)).toArray.sortBy { case (_, d) => d }
+  private[undersampling] val counter: Array[(Int, Int)] = this.y.groupBy((l: Int) => l).map((t: (Int, Array[Int])) => (t._1, t._2.length)).toArray.sortBy { case (_, d) => d }
   // In certain algorithms, reduce the minority class is forbidden, so let's detect what class is it if the user don't set one at pleasure
-  private[undersampling] val untouchableClass: Int = if (minorityClass == 0) this.counter.head._1 else if (minorityClass > this.randomizedY.length) throw new Exception("minorityClass is too big") else minorityClass
+  private[undersampling] val untouchableClass: Int = if (minorityClass == 0) this.counter.head._1 else if (minorityClass > this.y.length) throw new Exception("minorityClass is too big") else minorityClass
   // Extra information to obtain the Imbalanced Ratio
   private[undersampling] val minorityElements: Int = this.counter.head._2
   private[undersampling] val majorityElements: Int = this.counter.tail.map((_: (Int, Int))._2).sum
   // Info to normalize the data
-  private[undersampling] val maxV: Array[Double] = this.randomizedX.transpose.map((x: Array[Double]) => x.max)
-  private[undersampling] val minV: Array[Double] = this.randomizedX.transpose.map((x: Array[Double]) => x.min)
+  private[undersampling] val maxV: Array[Double] = this.x.transpose.map((x: Array[Double]) => x.max)
+  private[undersampling] val minV: Array[Double] = this.x.transpose.map((x: Array[Double]) => x.min)
   // Normalize the data as follow: for each column, x, (x-min(x))/(max(x)-min(x))
-  private[undersampling] var normalizedData: Array[Array[Double]] = (this.randomizedX.transpose zip (this.minV zip this.maxV)).map((row: (Array[Double], (Double, Double))) =>
+  private[undersampling] var normalizedData: Array[Array[Double]] = (this.x.transpose zip (this.minV zip this.maxV)).map((row: (Array[Double], (Double, Double))) =>
     row._1.map((e: Double) => e - row._2._1 / (row._2._2 / row._2._1))).transpose
+  // Shuffle the data to make it random
+  private[undersampling] var random: Random = new util.Random(seed)
+  private[undersampling] var index: List[Int] = this.random.shuffle(this.y.indices.toList)
+  private[undersampling] var randomizedX: Array[Array[Double]] = (this.index map this.normalizedData).toArray
+  private[undersampling] var randomizedY: Array[Int] = (this.index map this.y).toArray
 
   /** Shuffle the data
     *
