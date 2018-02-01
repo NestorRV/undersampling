ec6fea3fa60188f4e7059bb50d50df54b69f773e
Néstor Rodríguez <nestorrodriguezvico@gmail.com>
2018-02-01 01:32:12 +0100

    First steps in NeighborhoodCleaningRule class.

--- src/main/scala/undersampling/MainFile.scala
+++ src/main/scala/undersampling/MainFile.scala
@@ -32,12 +32,17 @@ object MainFile {
       val result: (Array[Array[Double]], Array[Int], Array[Int]) = cnn.sample(file = Option("./data/logs/" + dataset._1), distance = Distances.EUCLIDEAN)
       val attributeDataset: AttributeDataset = writer.toDataSet(data, result._1, result._2)
       writer.writeArff(attributeDataset, "./data/results/" + dataset._1 + "_CNN")
-      */
 
       val enn = new EditedNearestNeighbor(x, y)
       val result: (Array[Array[Double]], Array[Int], Array[Int]) = enn.sample(file = Option("./data/logs/" + dataset._1), distance = Distances.EUCLIDEAN, k = 3)
       val attributeDataset: AttributeDataset = writer.toDataSet(data, result._1, result._2)
       writer.writeArff(attributeDataset, "./data/results/" + dataset._1 + "_ENN")
+      */
+
+      val ncl = new NeighborhoodCleaningRule(x, y)
+      val result: (Array[Array[Double]], Array[Int], Array[Int]) = ncl.sample(file = Option("./data/logs/" + dataset._1), distance = Distances.EUCLIDEAN)
+      val attributeDataset: AttributeDataset = writer.toDataSet(data, result._1, result._2)
+      writer.writeArff(attributeDataset, "./data/results/" + dataset._1 + "_NCL")
     }
   }
 }
---
+++ src/main/scala/undersampling/core/NeighborhoodCleaningRule.scala
@@ -0,0 +1,81 @@
+package undersampling.core
+
+import undersampling.util.Utilities.{Distances, nnRule}
+
+import scala.collection.mutable.ArrayBuffer
+
+/** Neighborhood Cleaning Rule
+  *
+  * @param x             data to work with
+  * @param y             labels of the data associated with x
+  * @param seed          seed to use. If it is not provided, it will use the system time
+  * @param minorityClass integer representing the class to keep it untouched
+  * @author Néstor Rodríguez Vico
+  */
+class NeighborhoodCleaningRule(override private[undersampling] val x: Array[Array[Double]],
+                               override private[undersampling] val y: Array[Int],
+                               override private[undersampling] val seed: Long = System.currentTimeMillis(),
+                               minorityClass: Int = 0) extends Algorithm(x, y, seed, minorityClass) {
+
+  /** Compute the Neighborhood Cleaning Rule (NCL)
+    *
+    * @param file     file to store the log. If its set to None, log process would not be done
+    * @param distance distance to use when calling the NNRule algorithm
+    * @param k        number of neighbors to use when computing k-NN rule (normally 3 neighbors)
+    * @return reduced data, reduced labels, index of elements kept
+    */
+  def sample(file: Option[String] = None, distance: Distances.Distance, k: Int = 3): (Array[Array[Double]], Array[Int], Array[Int]) = {
+    if (file.isDefined) {
+      this.logger.setNames(List("DATA SIZE REDUCTION INFORMATION", "IMBALANCED RATIO", "REDUCTION PERCENTAGE"))
+      this.logger.addMsg("DATA SIZE REDUCTION INFORMATION", "ORIGINAL SIZE: %d".format(this.normalizedData.length))
+      this.logger.addMsg("IMBALANCED RATIO", "ORIGINAL: %s".format(imbalancedRatio(this.counter)))
+    }
+
+    // index of the element of the interest class
+    val indexC: Array[Int] = this.randomizedY.indices.toArray.filter((label: Int) => this.randomizedY(label) == this.untouchableClass)
+    // index of the rest
+    val indexO: Array[Int] = this.randomizedY.indices.toArray.diff(indexC.toList)
+
+    // look for noisy elements in O
+    val enn = new EditedNearestNeighbor(indexO map this.randomizedX, indexO map this.randomizedY)
+    val resultENN: (Array[Array[Double]], Array[Int], Array[Int]) = enn.sample(file = None, distance = Distances.EUCLIDEAN, k = k)
+    // noisy elements are the ones that are removed
+    val indexA1: Array[Int] = this.randomizedY.indices.toList.diff(resultENN._3.toList).toArray
+
+    val indexA2: ArrayBuffer[Int] = new ArrayBuffer[Int](0)
+    // TODO REPLACE ALL COUNTERS
+    // get the size of all the classes
+    val sizeOfClasses: Array[Int] = this.randomizedY.groupBy(identity).mapValues((_: Array[Int]).length).toArray.sortBy((count: (Int, Int)) => count._1).collect { case (c, s) => s }
+    val sizeC: Int = indexC.length
+
+    // search for eleements in O that misclassify elements in C
+    for (index <- indexC) {
+      // try to classify all the elements in C using O
+      val label: (Int, Option[Array[Int]]) = nnRule(data = indexO map this.randomizedX, labels = indexO map this.randomizedY,
+        newInstance = this.randomizedX(index), newInstanceLabel = this.randomizedY(index), k = k, distance = distance, getIndex = true)
+
+      // if is misclassified
+      if (label._1 != this.randomizedY(index)) {
+        // get the neighbours
+        val neighbors: Array[Int] = label._2.get
+        // and their classes
+        val neighborsClasses: Array[Int] = neighbors.map((n: Int) => this.randomizedY(n))
+        // and check if the size of theses classes is greater or equal than 0.5 * sizeC
+        val shouldBeAdded: Array[Boolean] = neighborsClasses.collect { case c if sizeOfClasses(c) >= (0.5 * sizeC) => true }
+
+        // add the neighbours that pass the test to indexA2
+        for (pair <- neighbors zip shouldBeAdded) {
+          if (pair._2) {
+            indexA2 += pair._1
+          }
+        }
+      }
+    }
+
+    // final index is allData - (indexA1 union indexA2)
+    val finalIndex: Array[Int] = randomizedY.indices.toArray.diff((indexA1.toList ++ indexA2.toArray).distinct.toArray)
+    // TODO log process
+    // TODO return unnormalized data
+    (finalIndex map this.randomizedX, finalIndex map this.randomizedY, finalIndex)
+  }
+}
