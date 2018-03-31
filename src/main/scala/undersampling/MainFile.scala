7a57816d9dba2e9eb788c8cdff9b3ed738b0ee61
Néstor Rodríguez <nestorrodriguezvico@gmail.com>
2018-03-31 19:45:39 +0200

    Fix: typo in main.

--- src/main/scala/undersampling/MainFile.scala
+++ src/main/scala/undersampling/MainFile.scala
@@ -83,57 +83,57 @@ object MainFile {
       val cpm = new ClassPurityMaximization(d, seed = 0L)
       println("ClassPurityMaximization")
       val resultCPM: Data = cpm.sample(file = Option("./input/logs/" + dataset), distance = Distances.EUCLIDEAN)
-      // writer.writeDelimitedText(file = "./input/results/" + dataset + "_CPM.data", data = resultCPM)
+      // writer.writeDelimitedText(file = "./input/results/" + dataset + "_CPM.arff", data = resultCPM)
 
       val cOSS = new ClusterOSS(d, seed = 0L)
       println("ClusterOSS")
       val resultCOSS: Data = cOSS.sample(file = Option("./input/logs/" + dataset), distance = Distances.EUCLIDEAN, numClusters = 50)
-      // writer.writeArff(file = "./input/results/" + dataset + "_ClusterOSS.data", data = resultCOSS)
+      // writer.writeArff(file = "./input/results/" + dataset + "_ClusterOSS.arff", data = resultCOSS)
 
       val cnn = new CondensedNearestNeighbor(d, seed = 0L)
       println("CondensedNearestNeighbor")
       val resultCNN: Data = cnn.sample(file = Option("./input/logs/" + dataset), distance = Distances.EUCLIDEAN)
-      // writer.writeArff(file = "./input/results/" + dataset + "_CNN.data", data = resultCNN)
+      // writer.writeArff(file = "./input/results/" + dataset + "_CNN.arff", data = resultCNN)
 
       val ee = new EasyEnsemble(d, seed = 0L)
       println("EasyEnsemble")
       val resultEE: Data = ee.sample(file = Option("./input/logs/" + dataset), n_times = 5)
-      // writer.writeArff(file = "./input/results/" + dataset + "_EE.data", data = resultEE)
+      // writer.writeArff(file = "./input/results/" + dataset + "_EE.arff", data = resultEE)
 
       val enn = new EditedNearestNeighbor(d, seed = 0L)
       println("EditedNearestNeighbor")
       val resultENN: Data = enn.sample(file = Option("./input/logs/" + dataset), distance = Distances.EUCLIDEAN)
-      // writer.writeArff(file = "./input/results/" + dataset + "_ENN.data", data = resultENN)
+      // writer.writeArff(file = "./input/results/" + dataset + "_ENN.arff", data = resultENN)
 
       val nm = new NearMiss(d, seed = 0L)
       println("NearMiss")
-      val resultNM: Data = nm.sample(file = Option("./input/logs/" + dataset), distance = Distances.EUCLIDEAN, version = 3, n_neighbours = 3, ratio = 1.0)
-      // writer.writeArff(file = "./input/results/" + dataset + "_NM.data", data = resultNM)
+      val resultNM: Data = nm.sample(file = Option("./input/logs/" + dataset), distance = Distances.EUCLIDEAN, version = 3, n_neighbours = 3)
+      // writer.writeArff(file = "./input/results/" + dataset + "_NM.arff", data = resultNM)
 
       val ncl = new NeighbourhoodCleaningRule(d, seed = 0L)
       println("NeighbourhoodCleaningRule")
       val resultNCL: Data = ncl.sample(file = Option("./input/logs/" + dataset), distance = Distances.EUCLIDEAN)
-      // writer.writeArff(file = "./input/results/" + dataset + "_NCL.data", data = resultNCL)
+      // writer.writeArff(file = "./input/results/" + dataset + "_NCL.arff", data = resultNCL)
 
       val oss = new OneSideSelection(d, seed = 0L)
       println("OneSideSelection")
       val resultOSS: Data = oss.sample(file = Option("./input/logs/" + dataset), distance = Distances.EUCLIDEAN)
-      // writer.writeArff(file = "./input/results/" + dataset + "_OSS.data", data = resultOSS)
+      // writer.writeArff(file = "./input/results/" + dataset + "_OSS.arff", data = resultOSS)
 
       val ru = new RandomUndersampling(d, seed = 0L)
       println("RandomUndersampling")
-      val resultRU: Data = ru.sample(file = Option("./input/logs/" + dataset), ratio = 1.0)
-      // writer.writeArff(file = "./input/results/" + dataset + "_RU.data", data = resultRU)
+      val resultRU: Data = ru.sample(file = Option("./input/logs/" + dataset))
+      // writer.writeArff(file = "./input/results/" + dataset + "_RU.arff", data = resultRU)
 
       val tl = new TomekLink(d, seed = 0L)
       println("TomekLink")
       val resultTL: Data = tl.sample(file = Option("./input/logs/" + dataset), distance = Distances.EUCLIDEAN)
-      // writer.writeArff(file = "./input/results/" + dataset + "_TL.data", data = resultTL)
+      // writer.writeArff(file = "./input/results/" + dataset + "_TL.arff", data = resultTL)
 
       val sbc = new UndersamplingBasedClustering(d, seed = 0L)
       println("UndersamplingBasedClustering")
       val resultSBC: Data = sbc.sample(file = Option("./input/logs/" + dataset), numClusters = 25)
-      // writer.writeArff(file = "./input/results/" + dataset + "_SBC.data", data = resultSBC)
+      // writer.writeArff(file = "./input/results/" + dataset + "_SBC.arff", data = resultSBC)
     }
   }
 }
