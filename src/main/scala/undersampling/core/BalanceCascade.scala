86f57d7ac204f7df2e7086da9e3dcd5dc9539f8e
Néstor Rodríguez <nestorrodriguezvico@gmail.com>
2018-05-31 16:43:51 +0200

    Combine the subsets in Balance Cascade.

--- src/main/scala/undersampling/core/BalanceCascade.scala
+++ src/main/scala/undersampling/core/BalanceCascade.scala
@@ -24,10 +24,14 @@ class BalanceCascade(private[undersampling] val data: Data,
     * @param k           number of neighbours to use when computing k-NN rule (normally 3 neighbours)
     * @param nMaxSubsets maximum number of subsets to generate
     * @param nFolds      number of subsets to create when applying cross-validation
+    * @param ratio       ratio to know how many majority class examples to preserve. By default it's set to 1 so there
+    *                    will be the same minority class examples as majority class examples. If it's set to 2, there
+    *                    will be the twice as many majority class examples as minority class examples
     * @return array of Data structures with all the important information and index of elements kept for each subset
     */
 
-  def sample(file: Option[String] = None, distance: Distances.Distance = Distances.EUCLIDEAN, k: Int = 3, nMaxSubsets: Int = 5, nFolds: Int = 5): Array[Data] = {
+  def sample(file: Option[String] = None, distance: Distances.Distance = Distances.EUCLIDEAN, k: Int = 3,
+             nMaxSubsets: Int = 5, nFolds: Int = 5, ratio: Double = 1.0): Data = {
     // Use randomized data
     val dataToWorkWith: Array[Array[Double]] = (this.index map this.x).toArray
     // and randomized classes to match the randomized data
@@ -50,7 +54,7 @@ class BalanceCascade(private[undersampling] val data: Data,
 
     while (search) {
       val indexToUnderSample: ArrayBuffer[Int] = new ArrayBuffer[Int](0)
-      val indexConstant: ArrayBuffer[Int] = new ArrayBuffer[Int](0)
+      val minorityIndex: ArrayBuffer[Int] = new ArrayBuffer[Int](0)
       val classesCounter: Map[Any, Int] = (boolToIndex(mask) map classesToWorkWith).groupBy(identity).mapValues((_: Array[Any]).length)
 
       classesCounter.foreach { target: (Any, Int) =>
@@ -61,12 +65,12 @@ class BalanceCascade(private[undersampling] val data: Data,
           val indexTargetClass: List[Int] = random.shuffle((indexClassInterest map classesToWorkWith).indices.toList).take(this.counter(this.untouchableClass))
           indexToUnderSample ++= (indexTargetClass map indexClassInterest)
         } else {
-          indexConstant ++= indexClass
+          minorityIndex ++= indexClass
         }
       }
 
       subsetsCounter += 1
-      val subset: Array[Int] = (indexToUnderSample ++ indexConstant).toArray
+      val subset: Array[Int] = (indexToUnderSample ++ minorityIndex).toArray
       subsets += subset
 
       val classesToWorkWithSubset: Array[Any] = subset map classesToWorkWith
@@ -81,44 +85,40 @@ class BalanceCascade(private[undersampling] val data: Data,
       classesToWorkWith.distinct.filter((c: Any) => c != this.untouchableClass).foreach((c: Any) => if (finalTargetStats(c) < this.counter(this.untouchableClass)) search = false)
     }
 
+    val minorityIndex: Array[Int] = subsets.flatten.filter((e: Int) => classesToWorkWith(e) == this.minorityClass).distinct.toArray
+    val majElements: Array[Int] = subsets.flatten.filter((e: Int) => classesToWorkWith(e) != this.minorityClass).toArray
+    val majorityIndexHistogram: Array[(Int, Int)] = majElements.groupBy(identity).mapValues((_: Array[Int]).length).toArray.sortBy((_: (Int, Int))._2).reverse
+    val majorityIndex: Array[Int] = majorityIndexHistogram.take((minorityIndex.length * ratio).toInt).map((_: (Int, Int))._1)
+
+    // Get the index of the reduced data
+    val finalIndex: Array[Int] = minorityIndex ++ majorityIndex
+
     // Stop the time
     val finishTime: Long = System.nanoTime()
 
+    this.data._resultData = (finalIndex map this.index).sorted map this.data._originalData
+    this.data._resultClasses = (finalIndex map this.index).sorted map this.data._originalClasses
+    this.data._index = (finalIndex map this.index).sorted
+
     if (file.isDefined) {
+      // Recount of classes
+      val newCounter: Map[Any, Int] = (finalIndex map classesToWorkWith).groupBy(identity).mapValues((_: Array[Any]).length)
+
       this.logger.addMsg("ORIGINAL SIZE: %d".format(dataToWorkWith.length))
+      this.logger.addMsg("NEW DATA SIZE: %d".format(finalIndex.length))
+      this.logger.addMsg("REDUCTION PERCENTAGE: %s".format(100 - (finalIndex.length.toFloat / dataToWorkWith.length) * 100))
+
       this.logger.addMsg("ORIGINAL IMBALANCED RATIO: %s".format(imbalancedRatio(this.counter, this.untouchableClass)))
-      this.logger.addMsg("NUMBER OF SUBSETS: %d.\n".format(subsets.length))
-    }
+      // Recompute the Imbalanced Ratio
+      this.logger.addMsg("IMBALANCED RATIO: %s".format(imbalancedRatio(newCounter, this.untouchableClass)))
 
-    val finalData: Array[Data] = subsets.zipWithIndex.map { (subset: (Array[Int], Int)) =>
-      val newData: Data = new Data(_nominal = this.data._nominal, _originalData = this.data._originalData, _originalClasses = this.data._originalClasses, _fileInfo = this.data._fileInfo)
-
-      newData._resultData = (subset._1 map this.index).sorted map this.data._originalData
-      newData._resultClasses = (subset._1 map this.index).sorted map this.data._originalClasses
-      newData._index = (subset._1 map this.index).sorted
-
-      if (file.isDefined) {
-        // Recount of classes
-        val newCounter: Map[Any, Int] = (subset._1 map classesToWorkWith).groupBy(identity).mapValues((_: Array[Any]).length)
-        this.logger.addMsg("NEW DATA SIZE OF SUBSET %s: %d".format(subset._2, subset._1.length))
-        this.logger.addMsg("REDUCTION PERCENTAGE OF SUBSET %s: %s".format(subset._2, 100 - (subset._1.length.toFloat / dataToWorkWith.length) * 100))
-        // Recompute the Imbalanced Ratio
-        this.logger.addMsg("IMBALANCED RATIO OF SUBSET %s: %s".format(subset._2, imbalancedRatio(newCounter, this.untouchableClass)))
-      }
-
-      newData
-    }.toArray
-
-    if (file.isDefined) {
-      // Save the distance calculation time
-      this.logger.addMsg("\nDISTANCES CALCULATION TIME: %s".format(nanoTimeToString(distancesTime)))
       // Save the time
       this.logger.addMsg("TOTAL ELAPSED TIME: %s".format(nanoTimeToString(finishTime - initTime)))
 
       // Save the log
-      this.logger.storeFile(file.get + "_BC")
+      this.logger.storeFile(file.get + "_EE")
     }
 
-    finalData
+    this.data
   }
 }
